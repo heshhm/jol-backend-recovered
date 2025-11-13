7fb9b530d94b4ac5ee851d03a9ff29453396be0a
heshhm <hesham.rectify@gmail.com>
2025-11-13 19:24:38 +0500

    clean up

--- src/api/schema.py
+++ src/api/schema.py
@@ -1,14 +1,9 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 from django.urls import path, re_path
 
-schema_view = get_schema_view()
-
-
-
-get_schema_view(
+schema_view = get_schema_view(
     openapi.Info(
         title="JOl Game API",
         default_version='v1',
--- src/api/urls.py
+++ src/api/urls.py
@@ -4,6 +4,5 @@ from .schema import schema_urls
 urlpatterns = [
     path("auth/", include("src.api.auth.urls")),
     path("auth/registration/", include("dj_rest_auth.registration.urls")),
-
     path("v1/", include("src.api.v1.urls")),
 ] + schema_urls
