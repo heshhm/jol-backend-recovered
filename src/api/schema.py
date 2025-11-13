37937a54ed4982382a74fd3edcc1bc1bf6413e8e
heshhm <hesham.rectify@gmail.com>
2025-11-13 19:15:31 +0500

    api schema

---
+++ src/api/schema.py
@@ -0,0 +1,28 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.urls import path, re_path
+
+schema_view = get_schema_view()
+
+
+
+get_schema_view(
+    openapi.Info(
+        title="JOl Game API",
+        default_version='v1',
+        description="API documentation for Jaaloo backend",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+schema_urls = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
--- src/api/urls.py
+++ src/api/urls.py
@@ -1,31 +1,9 @@
-from django.urls import path, include, re_path
-
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="JOl Game API",
-        default_version='v1',
-        description="API documentation for Jaaloo backend",
-        terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
+from django.urls import path, include
+from .schema import schema_urls
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
     path("auth/", include("src.api.auth.urls")),
     path("auth/registration/", include("dj_rest_auth.registration.urls")),
 
     path("v1/", include("src.api.v1.urls")),
-]
+] + schema_urls
